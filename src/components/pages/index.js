e1be2b62b709555301797dcb28257f3db708ca99
Musiichenko Dmitry <dmitrymusiichenko@gmail.com>
2022-11-07 12:48:16 +0200

    Updates

--- src/components/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Main from "./Main";
-import Comics from "./Comics";
-import Comic from "./Comic";
-import NotFound from "./NotFound";
-import Character from "./Character";
-
-export  { Main, Comics, Comic, NotFound, Character }
